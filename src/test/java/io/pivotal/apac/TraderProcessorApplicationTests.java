d1f45ecf31cdbe6d084beff3df0118306fdd6a8d
Sergiu <sbodiu@pivotal.io>
2016-05-08 23:57:47 +0800

    minor updates

--- src/test/java/io/pivotal/apac/TraderProcessorApplicationTests.java
+++ src/test/java/io/pivotal/apac/TraderProcessorApplicationTests.java
@@ -6,11 +6,25 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringApplicationConfiguration(classes = TestApplication.class)
+@WebAppConfiguration
 public class TraderProcessorApplicationTests {
+	@Autowired
+	private Processor processor;
+
+	@Autowired
+	private MessageCollector collector;
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {
+		processor.input().send(MessageBuilder
+				.withPayload(new Payment("foo", "bar", Money.parse("GBP1000"))).build());
+		assertThat(collector.forChannel(processor.output()).poll(1000,
+				TimeUnit.MILLISECONDS), is(notNullValue()));
 	}
 
+	@Configuration
+	@Import({TestSupportBinderAutoConfiguration.class, ProcessorApplication.class})
+	protected static class TestApplication {
+	}
 }
