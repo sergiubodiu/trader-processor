59097356e0960450f3d0258952cb9974505c4325
Sergiu <sbodiu@pivotal.io>
2016-05-09 00:27:40 +0800

    minor updates

--- src/main/java/io/pivotal/apac/TraderProcessorApplication.java
+++ src/main/java/io/pivotal/apac/TraderProcessorApplication.java
@@ -1,12 +1,124 @@
 package io.pivotal.apac;
 
+import org.joda.money.Money;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.integration.annotation.Router;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.annotation.Transformer;
 
-@SpringBootApplication
-public class TraderProcessorApplication {
+	@SpringBootApplication
+	@EnableBinding(Processor.class)
+	@MessageEndpoint
+	public class TraderProcessorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TraderProcessorApplication.class, args);
+		private static Logger log = LoggerFactory.getLogger(TraderProcessorApplication.class);
+
+		@Transformer(inputChannel = Processor.INPUT, outputChannel = "payments")
+		public Payment enhance(Payment inbound) {
+			// TODO: do stuff to the payment
+			return inbound;
+		}
+
+		@Router(inputChannel = "payments")
+		public String route(Payment inbound) {
+			return "fp";
+		}
+
+		@ServiceActivator(inputChannel = "fp", outputChannel = Processor.OUTPUT)
+		public FastPayment pay(Payment inbound) {
+			log.info("Fast payment: " + inbound);
+			return new FastPayment(inbound.getMsg(), inbound.getId(), inbound.getAmount());
+		}
+
+		public static void main(String[] args) {
+			SpringApplication.run(TraderProcessorApplication.class, args);
+		}
 	}
-}
+
+	class Payment {
+
+		private String msg;
+		private Money amount;
+		private String id;
+
+		@SuppressWarnings("unused")
+		private Payment() {
+		}
+
+		public Payment(String msg, String id, Money amount) {
+			this.msg = msg;
+			this.id = id;
+			this.amount = amount;
+		}
+
+		public String getMsg() {
+			return msg;
+		}
+
+		public void setMsg(String msg) {
+			this.msg = msg;
+		}
+
+		public Money getAmount() {
+			return amount;
+		}
+
+		public String getId() {
+			return id;
+		}
+
+		@Override
+		public String toString() {
+			return "Payment [id=" + id + ", amount=" + amount + ", msg="
+					+ msg.substring(0, Math.min(40, msg.length())).replaceAll("\n", " ")
+					+ "]";
+		}
+
+	}
+
+	class FastPayment {
+
+		private String msg;
+		private Money amount;
+		private String id;
+
+		@SuppressWarnings("unused")
+		private FastPayment() {
+		}
+
+		public FastPayment(String msg, String id, Money amount) {
+			this.msg = msg;
+			this.id = id;
+			this.amount = amount;
+		}
+
+		public String getMsg() {
+			return msg;
+		}
+
+		public void setMsg(String msg) {
+			this.msg = msg;
+		}
+
+		@Override
+		public String toString() {
+			return "FastPayment [id=" + id + ", amount=" + amount + ", msg="
+					+ msg.substring(0, Math.min(40, msg.length())).replaceAll("\n", " ")
+					+ "]";
+		}
+
+		public Money getAmount() {
+			return amount;
+		}
+
+		public String getId() {
+			return id;
+		}
+
+	}
--- src/test/java/io/pivotal/apac/TraderProcessorApplicationTests.java
+++ src/test/java/io/pivotal/apac/TraderProcessorApplicationTests.java
@@ -1,12 +1,29 @@
 package io.pivotal.apac;
 
+import org.joda.money.Money;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.cloud.stream.test.binder.MessageCollector;
+import org.springframework.cloud.stream.test.binder.TestSupportBinderAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringApplicationConfiguration(classes = TestApplication.class)
+@SpringBootTest(classes = TraderProcessorApplicationTests.TestApplication.class)
 @WebAppConfiguration
 public class TraderProcessorApplicationTests {
 	@Autowired
@@ -15,16 +32,26 @@ public class TraderProcessorApplicationTests {
 	@Autowired
 	private MessageCollector collector;
 
+    Money amount = Money.parse("GBP1000");
+    String msg = "foo";
+    String id = "bar";
+
 	@Test
 	public void contextLoads() throws Exception {
-		processor.input().send(MessageBuilder
-				.withPayload(new Payment("foo", "bar", Money.parse("GBP1000"))).build());
-		assertThat(collector.forChannel(processor.output()).poll(1000,
-				TimeUnit.MILLISECONDS), is(notNullValue()));
-	}
+
+        processor.input().send(MessageBuilder
+				.withPayload(new Payment(msg, id, amount)).build());
+
+        Message<?> message = collector.forChannel(processor.output()).poll(1000, TimeUnit.MILLISECONDS);
+        assertThat(message, is(notNullValue()));
+        FastPayment payment = (FastPayment) message.getPayload();
+        assertEquals(payment.getAmount(), amount);
+        assertEquals(payment.getMsg(), msg);
+        assertEquals(payment.getId(), id);
+    }
 
 	@Configuration
-	@Import({TestSupportBinderAutoConfiguration.class, ProcessorApplication.class})
+	@Import({TestSupportBinderAutoConfiguration.class, TraderProcessorApplication.class})
 	protected static class TestApplication {
 	}
 }
