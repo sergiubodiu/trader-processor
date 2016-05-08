b3c97ed2e41e862d3e1fe5b4c3d6a6bfdcf07211
Sergiu <sbodiu@pivotal.io>
2016-05-07 23:47:09 -0800

    minor updates

---
+++ src/main/java/io/pivotal/apac/TraderProcessorApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.apac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraderProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraderProcessorApplication.class, args);
+	}
+}
